d8762a13eac8739acd6d0851a6ab4af7c29e9952
Nicolas Ochsner <nicolasochsner@gmail.com>
2025-04-21 14:46:23 +0200

    Created lock file with yarn

--- flake.nix
+++ flake.nix
@@ -21,10 +21,42 @@
       flake-utils,
       rust-overlay,
     }:
+    let
+      patch-overlay =
+        final: prev:
+        let
+          node-patcher = (
+            prevAttrs: {
+              patches = prevAttrs.patches ++ [
+                (prev.fetchpatch2 {
+                  url = "https://github.com/nodejs/node/commit/33f6e1ea296cd20366ab94e666b03899a081af94.patch?full_index=1";
+                  hash = "sha256-aVBMcQlhQeviUQpMIfC988jjDB2BgYzlMYsq+w16mzU=";
+                })
+              ];
+            }
+          );
+        in
+        # this creates an attribute set with
+        # {
+        #   nodejs_19 = prev.nodejs_19.overrideAttrs node-patcher;
+        #   [...]
+        # }
+        prev.lib.genAttrs [
+          "nodejs_19"
+          "nodejs_20"
+          "nodejs_22"
+          "nodejs_23"
+          "nodejs-slim"
+          "nodejs-slim_18"
+          "nodejs-slim_22"
+          "nodejs-slim_23"
+        ] (name: prev."${name}".overrideAttrs node-patcher);
+    in
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         overlays = [
+          patch-overlay
           (import rust-overlay)
         ];
         pkgs = import nixpkgs {
@@ -44,6 +76,7 @@
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             rust-select
+            yarn
           ];
         };
       }
